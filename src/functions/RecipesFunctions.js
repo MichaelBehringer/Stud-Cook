502131f4a62a34258b519b7a690f4da3d19e24db
Michael Behringer <Michael.Behringer@hama.de>
2022-03-07 21:47:13 +0100

    background image, move between recipes

---
+++ src/functions/RecipesFunctions.js
@@ -0,0 +1,41 @@
+export function getRecipeForID(searchID) {
+	const recipes = require('../data/Recipes.json');
+
+	return recipes.filter(recipe => recipe.id == searchID)[0];
+}
+
+export function getNextRecipe(searchID) {
+	const recipes = require('../data/Recipes.json');
+
+	let returnRecipe = {};
+
+	recipes.filter((recipe, idx) => {
+		if(recipe.id==searchID) {
+			if((idx+1)==recipes.length) {
+				returnRecipe =  recipes[0]
+			} else {
+				returnRecipe = recipes[idx+1]
+			}
+		}
+	});
+
+	return returnRecipe;
+}
+
+export function getPreviousRecipe(searchID) {
+	const recipes = require('../data/Recipes.json');
+
+	let returnRecipe = {};
+
+	recipes.filter((recipe, idx) => {
+		if(recipe.id==searchID) {
+			if(idx==0) {
+				returnRecipe =  recipes[recipes.length-1]
+			} else {
+				returnRecipe = recipes[idx-1]
+			}
+		}
+	});
+
+	return returnRecipe;
+}
