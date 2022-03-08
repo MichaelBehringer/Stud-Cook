2dd7420cad8a55ef198710a00150ce87767795f4
Paul <paulbrilmayer@gmail.com>
2022-03-08 10:04:21 +0100

    PDF down geht jetz

--- src/Views/ShoppingList.js
+++ src/Views/ShoppingList.js
@@ -3,27 +3,30 @@ import "./ShoppingList.css";
 import jsPDF from "jspdf"
 import logo from "../images/name.png"
 
-function pdfGenerate(){
+function pdfGenerate(inputText){
   var doc = new jsPDF("portrait")
   doc.addImage(logo, "PNG", 0, 0, 100, 20);
-  doc.text("\n \n \n Test Text", 10, 10)
+  doc.text(inputText, 10, 30)
   doc.save("Einkaufsliste.pdf")
 }
 
 function ShoppingList() {
+  let pdfInt=[]
   var recipes = require('../data/Recipes.json');
   const [, updateState] = React.useState();
   const forceUpdate = React.useCallback(() => updateState({}), []);
   let stringShoppingList = localStorage.getItem('shoppingList')
   return (
     <div className="card cardMain">
-      <h1>Einkaufsliste</h1>
+      <h1>Einkaufsliste<button className="colorThemeDeleteButton" onClick={() => { localStorage.removeItem('shoppingList'); forceUpdate() }}>Einkaufsliste löschen 🗑</button></h1>
       {stringShoppingList ?
         <div>
           <ul>
             {recipes.map((recipe) => {
               if (JSON.parse(stringShoppingList).shoppingList.includes(recipe.id)) {
-                return recipe.ingredients.map((ing) => <div className="backForListForInt"> <li className="listForInt" key={ing.name} message={ing.name} >{ing.name + ' ' + ing.amounth + ing.scale}</li></div>)
+                recipe.ingredients.map((ing) => pdfInt.push(ing.name + ' ' + ing.amounth + ing.scale))
+                console.log(pdfInt)
+                return recipe.ingredients.map((ing) => <li className="listForInt" key={ing.name} message={ing.name} >{ing.name + ' ' + ing.amounth + ing.scale}</li>)
               }})}
 
           </ul>
@@ -33,13 +36,12 @@ function ShoppingList() {
           <h1>Einkaufsliste ist leer</h1>
         </div>
       }
-      <div style={{ textAlign: "center" }}>
-        <button className="prettybutton" onClick={() => { localStorage.removeItem('shoppingList'); forceUpdate() }}>Einkaufsliste leeren</button>
-        <br></br>
-        <button className="prettybutton" onClick={pdfGenerate}>PDF TEST</button>
+      <div>
+        <button className="colorThemeButton marginTopButtonTemp" onClick={() => pdfGenerate(pdfInt.join('\n'))}>Einkaufsliste als PDF herunterladen</button>
       </div>
     </div>
   );
 }
+
 
 export default ShoppingList;
