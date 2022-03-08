5ec51db460766ff172e25553739b5082b23b10de
windrathl <101147752+windrathl@users.noreply.github.com>
2022-03-08 09:35:48 +0100

    Update Impressum.js

--- src/Views/Impressum.js
+++ src/Views/Impressum.js
@@ -5,7 +5,11 @@ function Impressum() {
     <div>
       <div className="card cardMain">
         <h1>Impressum</h1>
-        
+        <p>Anbieter:<br />Adamicki Karolina, Behringer Michael, Brilmayer Paul, Löhr Max, Windrath Lukas<br />Marienstraße 20<br />89518 Heidenheim an der Brenz</p>
+        <p>Kontakt:<br />Telefon: 07321/2722138<br />E-Mail: <a href="mailto:behringerm.tin21@student.dhbw-heidenheim.de" >behringerm.tin21@student.dhbw-heidenheim.de</a><br />Website: <a href="https://stud-cook.web.app/" >stud-cook.web.app</a></p>
+        <p> </p>
+        <p>Bei redaktionellen Inhalten:</p>
+        <p>Verantwortlich nach § 55 Abs.2 RStV<br />DHBW Heidenheim<br />Marienstraße 20 <br />89518 Heidenheim an der Brenz</p>
       </div>
     </div>
   );
