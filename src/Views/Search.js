5d5877cfbc439262c54d57a9455a3436c096cc49
Max <maxlh739gmail.com>
2022-03-09 13:05:37 +0100

    update search

--- src/Views/Search.js
+++ src/Views/Search.js
@@ -1,17 +1,67 @@
 import React from "react";
 import {useNavigate } from 'react-router-dom';
+import SearchData from '../data/Recipes.json';
+import {useState} from 'react';
+
 
 function Search() {
-  const navigate = useNavigate();
-  var recipes = require('../data/Recipes.json');
   return (
-    <div>
-      <div className="card cardMain">
-        <h1>Suche</h1>
-        {recipes.map((recipe) => <button className="colorThemeButton searchButton" key={recipe.id} onClick={() => navigate('/detail/' + recipe.id)}>{recipe.name}</button>)}
-      </div>
+    <div className="App">
+      <SearchBar placeholder="Search..." data={SearchData} />
     </div>
   );
 }
 
+
+function SearchBar({ placeholder, data }) {
+  const [filteredData, setFilteredData] = useState([]);
+  const [wordEntered, setWordEntered] = useState("");
+  const navigate = useNavigate();
+  let recipes = require('../data/Recipes.json');
+
+  const handleFilter = (event) => {
+    const searchWord = event.target.value;
+    setWordEntered(searchWord);
+    const newFilter = data.filter((value) => {
+      return value.name.toLowerCase().includes(searchWord.toLowerCase());
+    });
+
+    if (searchWord === "") {
+      setFilteredData([]);
+    } else {
+      setFilteredData(newFilter);
+    }
+  };
+
+  const clearInput = () => {
+    setFilteredData([]);
+    setWordEntered("");
+  };
+
+  return (
+    <div className="search">
+      <div className="searchInputs">
+        <input
+          type="text"
+          placeholder={placeholder}
+          value={wordEntered}
+          onChange={handleFilter}
+        />
+        
+      </div>
+      {filteredData.length != 0 && (
+        <div className="dataResult">
+          {filteredData.slice(0, 15).map((value, key) => {
+            return (
+              <a className="dataItem" key={value.id} onClick={() => navigate('/detail/' + value.id)}>
+                <p>{value.name} </p>
+              </a>
+            );
+          })}
+        </div>
+      )}
+      {recipes.map((recipe) => <button className="colorThemeButton searchButton" key={recipe.id} onClick={() => navigate('/detail/' + recipe.id)}>{recipe.name}</button>)}
+    </div>
+  );
+}
 export default Search;
