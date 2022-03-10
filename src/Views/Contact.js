5b557953f0d0a4c30bbb80be36f181ccadcb6780
windrathl <lukas.windrath@gmx.de>
2022-03-10 16:49:15 +0100

    formsubmit name in input hinzugefügt

--- src/Views/Contact.js
+++ src/Views/Contact.js
@@ -8,19 +8,19 @@ function Contact() {
   <form action="https://formsubmit.co/stud.cook.dhbw@gmail.com" className="contact-form" method="POST">
   <input type="hidden" name="_next" value={window.location.href.replace('contact', 'emailSent')}></input>
       <div className="input-block" >
-        <input placeholder="Vorname" type="text" className="form-control" required/>
+        <input placeholder="Vorname" type="text" className="form-control" required name="name"/>
       </div>
       <div className="input-block">
-        <input placeholder="Nachname" type="text" className="form-control" required/>
+        <input placeholder="Nachname" type="text" className="form-control" required name="name"/>
       </div>
       <div className="input-block">
-        <input placeholder="E-Mail" type="email" className="form-control" required/>
+        <input placeholder="E-Mail" type="email" className="form-control" required name="email"/>
       </div>
       <div className="input-block">
-        <input placeholder="Betreff" type="text" className="form-control" required/>
+        <input placeholder="Betreff" type="text" className="form-control" required name="issue"/>
       </div>
       <div className="input-block textarea">
-        <textarea placeholder="Ihre Nachricht" rows="3" type="text" className="form-control" required></textarea>
+        <textarea placeholder="Ihre Nachricht" rows="3" type="text" className="form-control" required name="message"></textarea>
       </div>
       <button className="square-button" type="submit" id="submit-button">Senden</button>
   </form>
